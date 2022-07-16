6c8bd96ae5009b45edebb9f0285ce740b1c88339
mbcisse <mohamedbassirou1989@gmail.com>
2022-07-16 23:59:15 +0200

    commit_last16072022

--- two_min_flux_vehicules_drivers.py
+++ two_min_flux_vehicules_drivers.py
@@ -25,23 +25,23 @@ def get_last_update_date_alpha_taxis_ids(alpha_taxis_ids):
     for var in alpha_taxis_ids:
         
         odoo_last_update  = var["__last_update"]
-        odoo_last_update= datetime.strptime(odoo_last_update, "%Y-%m-%d %H:%M:%S") 
         db_last_update_alpha= half_flow_odoo_topivotdb.getting_alpha_taxis_db_last_update_from_pivotdb(var["id"])
-        
-        if db_last_update_alpha is not None and db_last_update_alpha !='false':
-            db_last_update_alpha= datetime.strptime(db_last_update_alpha, "%Y-%m-%d %H:%M:%S")
-            
-            if odoo_last_update > db_last_update_alpha:
-                #print("db_last_update: ",   db_last_update_alpha)
-                #print("odoo_last_update: ", odoo_last_update)
-                result.append(var)
+       
+        if odoo_last_update is not False and odoo_last_update != 'false':
+            if db_last_update_alpha is not None and db_last_update_alpha !='false':
+                db_last_update_alpha= datetime.strptime(db_last_update_alpha, "%Y-%m-%d %H:%M:%S")
+                odoo_last_update= datetime.strptime(odoo_last_update, "%Y-%m-%d %H:%M:%S") 
+                if odoo_last_update > db_last_update_alpha:
+                    return var
+                else:
+                    return []
+            elif db_last_update_alpha =='false' or db_last_update_alpha=='False':
+                return var
             else:
-                pass
-                #print("No need to update the data base")
+                return []
         else:
-            pass
-            #print("This id not found in the database: ", var["id"])
-    return result
+            return []
+    
 
 def get_alpha_taxis_with_ids(db_id):
     today = datetime.today() 
@@ -84,27 +84,20 @@ def get_fleet_vehicule_assignation(driver_ids):
         id_vehicule_prec=id_vehicule_actual= resul[0]['vehicle_id']
         start_date_vehic_prec=start_date_vehic_actual= resul[0]['date_start']
         end_date_vehic_prec= end_date_vehic_actual= resul[0]['date_end']
-        last_update= resul[0]['__last_update']
-        
+        last_update= resul[0]['__last_update']        
         odoo_last_update= resul[0]["__last_update"]
-        if db_last_update != 'false' and odoo_last_update  != 'false':
-            db_last_update  = datetime.strptime(db_last_update, "%Y-%m-%d %H:%M:%S")
-            odoo_last_update= datetime.strptime(odoo_last_update, "%Y-%m-%d %H:%M:%S")
-            print("db_last_update get_fleet_vehicule_assignation: ",   db_last_update)
-            print("odoo_last_update get_fleet_vehicule_assignation : ", odoo_last_update)
-        
-            if  odoo_last_update > db_last_update:
-                print("Yes odoo_last_update: ", odoo_last_update)
+        if odoo_last_update  != 'false':
+            if db_last_update != 'false':
+                db_last_update  = datetime.strptime(db_last_update, "%Y-%m-%d %H:%M:%S")
+                odoo_last_update= datetime.strptime(odoo_last_update, "%Y-%m-%d %H:%M:%S")                        
+                if  odoo_last_update > db_last_update:
+                    return  id_vehicule_prec[0], id_vehicule_actual[0], start_date_vehic_prec, start_date_vehic_actual, end_date_vehic_prec, end_date_vehic_actual, last_update,last_update
+                else:
+                    return []
+            elif db_last_update == 'false':
                 return  id_vehicule_prec[0], id_vehicule_actual[0], start_date_vehic_prec, start_date_vehic_actual, end_date_vehic_prec, end_date_vehic_actual, last_update,last_update
-            else:
-                pass
-
         elif odoo_last_update=='false':
             return[]
-        
-        elif db_last_update =='false' and odoo_last_update !='false':
-            return  id_vehicule_prec[0], id_vehicule_actual[0], start_date_vehic_prec, start_date_vehic_actual, end_date_vehic_prec, end_date_vehic_actual, last_update, last_update
-     
         else:
             return[]
     elif count > 1:
@@ -124,46 +117,34 @@ def get_fleet_vehicule_assignation(driver_ids):
         odoo_last_update_prec= resul[1]["__last_update"]
         
         if veh_to_take=='actuel':        
-            if db_last_update != 'false' and odoo_last_update  != 'false':
-                db_last_update  = datetime.strptime(db_last_update, "%Y-%m-%d %H:%M:%S")
-                odoo_last_update= datetime.strptime(odoo_last_update, "%Y-%m-%d %H:%M:%S")
-                print("db_last_update get_fleet_vehicule_assignation: ",   db_last_update)
-                print("odoo_last_update get_fleet_vehicule_assignation: ", odoo_last_update)
-                if  odoo_last_update > db_last_update:
-                    print("Yes odoo_last_update get_fleet_vehicule_assignation: ", odoo_last_update)
-                    return id_vehicule_prec[0], id_vehicule_actual[0], start_date_vehic_prec, start_date_vehic_actual, end_date_vehic_prec, end_date_vehic_actual,last_update_prec, last_update_prec
-                else:
-                    return[]
-            
-            elif odoo_last_update=='false':
-                return[]   
-        
-            elif db_last_update =='false' and odoo_last_update !='false':
-                return  id_vehicule_prec[0], id_vehicule_actual[0], start_date_vehic_prec, start_date_vehic_actual, end_date_vehic_prec, end_date_vehic_actual, last_update_prec, last_update_actual
+            if odoo_last_update  != 'false' and odoo_last_update != False:
+                if db_last_update != 'false':
+                    db_last_update  = datetime.strptime(db_last_update, "%Y-%m-%d %H:%M:%S")
+                    odoo_last_update= datetime.strptime(odoo_last_update, "%Y-%m-%d %H:%M:%S")
+                    if  odoo_last_update > db_last_update:
+                        return id_vehicule_prec[0], id_vehicule_actual[0], start_date_vehic_prec, start_date_vehic_actual, end_date_vehic_prec, end_date_vehic_actual,last_update_prec, last_update_prec
+                    else:
+                        return []
 
+                elif db_last_update == 'false':
+                    return id_vehicule_prec[0], id_vehicule_actual[0], start_date_vehic_prec, start_date_vehic_actual, end_date_vehic_prec, end_date_vehic_actual,last_update_prec, last_update_prec
+            elif odoo_last_update=='false' or odoo_last_update is False :
+                return[]   
         else:
             odoo_last_update= odoo_last_update_prec
-            if db_last_update != 'false' and odoo_last_update  != 'false':
-                db_last_update  = datetime.strptime(db_last_update, "%Y-%m-%d %H:%M:%S")
-                odoo_last_update= datetime.strptime(odoo_last_update, "%Y-%m-%d %H:%M:%S")
-                print("db_last_update: ",   db_last_update)
-                print("odoo_last_update: ", odoo_last_update)
-                if  odoo_last_update > db_last_update:
-                    print("Yes odoo_last_update: ", odoo_last_update)
+            if odoo_last_update  != 'false':
+                if db_last_update != 'false':
+                    db_last_update  = datetime.strptime(db_last_update, "%Y-%m-%d %H:%M:%S")
+                    odoo_last_update= datetime.strptime(odoo_last_update, "%Y-%m-%d %H:%M:%S")
+                    if  odoo_last_update > db_last_update:
+                        return id_vehicule_prec[0], id_vehicule_actual[0], start_date_vehic_prec, start_date_vehic_actual, end_date_vehic_prec, end_date_vehic_actual,last_update_prec, last_update_prec
+                    else:
+                        return[]
+                elif db_last_update == 'false':
                     return id_vehicule_prec[0], id_vehicule_actual[0], start_date_vehic_prec, start_date_vehic_actual, end_date_vehic_prec, end_date_vehic_actual,last_update_prec, last_update_prec
-                else:
-                    return[]
-            
-            elif odoo_last_update=='false':
-                return[]   
-        
-            elif db_last_update =='false' and odoo_last_update !='false':
-                return id_vehicule_prec[0], id_vehicule_actual[0], start_date_vehic_prec, start_date_vehic_actual, end_date_vehic_prec, end_date_vehic_actual,last_update_prec, last_update_prec
-
-               
+            elif odoo_last_update=='false' or odoo_last_update is False :
+                return[]      
     
-    else:
-        return False
     
 
 def get_alpha_taxis_datas(driver_ids):
@@ -191,10 +172,7 @@ def get_alpha_taxis_datas(driver_ids):
 
 def get_res_partner_with_ids(driver_id):
 
-    now =  datetime.now()
-    today_date=  now.strftime("%Y-%m-%d")
     db_x_studio_x_lastupdatedate_flow= half_flow_odoo_topivotdb.getting_db_x_studio_x_lastupdatedate__flow_from_pivotdb(driver_id)
-
     drivers_datas= models.execute_kw(odoo_acces.db, uid, odoo_acces.password, 'res.partner', 'search_read',
         [[('is_driver', '=', True), ('id', '=', driver_id)]], 
         {'fields': ['radio_code', 'alpha_taxis_ids', 'driver_lastname', 'driver_firstname',
@@ -215,6 +193,8 @@ def get_res_partner_with_ids(driver_id):
         elif db_x_studio_x_lastupdatedate_flow == 'false':
             print("Yes odoo_last_update: ", odoo_last_update)
             return drivers_datas[0]
+        else:
+            return []
 
     elif odoo_last_update == False or odoo_last_update is None:
         return []
@@ -224,25 +204,48 @@ def get_res_partner_with_ids(driver_id):
 def two_min_getting_drivers_vehicules_datas():
 
     drivers_ids=  half_flow_odoo_topivotdb.get_alpha_taxis_with_ids_from_pivotdb()
-    for elem in drivers_ids:
-        #print("Elem: ", elem[0])
-        drivers_ids_alpha=get_alpha_taxis_with_ids(elem[0])
-        drivers_ids_res_partner= get_res_partner_with_ids(elem[0])
-        drivers_fleet_vehicule = get_fleet_vehicule_assignation(elem[0])     
-        
-        if len(drivers_ids_alpha) > 0:
-            for datas_alpha_taxis in drivers_ids_alpha:
-                start_date_alpha_taxis= datas_alpha_taxis['start_date']
-                end_date_alpha_taxis= datas_alpha_taxis['end_date']
-                status= datas_alpha_taxis['status']
-                last_update_alpha= datas_alpha_taxis["__last_update"] 
-                driver_id= datas_alpha_taxis['id']   
-                half_flow_odoo_topivotdb.two_min_flux_drivers_alpha_taxis_datas_update(driver_id, start_date_alpha_taxis, 
+    for elem in drivers_ids:        
+        print("elem[0]: ", elem[0])
+        if get_alpha_taxis_with_ids(elem[0]) != [] and get_alpha_taxis_with_ids(elem[0]) is not None:
+            datas_alpha_taxis=get_alpha_taxis_with_ids(elem[0])
+            print("Got some updates to be done on datas_alpha_taxis: ", datas_alpha_taxis)
+            start_date_alpha_taxis= datas_alpha_taxis['start_date']
+            end_date_alpha_taxis= datas_alpha_taxis['end_date']
+            status= datas_alpha_taxis['status']
+            last_update_alpha= datas_alpha_taxis["__last_update"] 
+            driver_id= datas_alpha_taxis['id']   
+            half_flow_odoo_topivotdb.two_min_flux_drivers_alpha_taxis_datas_update(driver_id, start_date_alpha_taxis, 
             end_date_alpha_taxis, status, last_update_alpha) 
-            #print(var)
-        else:
-            print("No need to update")
+          
+        if get_res_partner_with_ids(elem[0]) != [] and get_res_partner_with_ids(elem[0]) is not None:  
+            print("Got some updates to be done on datas_res_partner")
+            datas_res_partner = get_res_partner_with_ids(elem[0])
+            driver_id= datas_res_partner['id']
+            driver_last_name= datas_res_partner['driver_lastname']
+            driver_first_name= datas_res_partner['driver_firstname']
+            email= datas_res_partner['email']
+            mobile= datas_res_partner['mobile']
+            adresse= str(datas_res_partner['street']) + " " +  str(datas_res_partner['zip']) +  str(datas_res_partner['zip']) + " " + str(datas_res_partner['city'])
+            group_client_id= datas_res_partner['ref']
+            display_name= datas_res_partner['display_name']         
+            radio_code= datas_res_partner['radio_code']
+            alpha_taxis_ids= datas_res_partner['alpha_taxis_ids']
+            lastupdatedate_res_partner= datas_res_partner['x_studio_x_lastupdatedate_flow']
+            half_flow_odoo_topivotdb.two_min_flux_drivers_res_partner_datas_update( 
+              driver_id, radio_code, alpha_taxis_ids,driver_last_name,  driver_first_name, email, mobile, adresse, 
+              group_client_id, display_name, lastupdatedate_res_partner)
 
+        if get_fleet_vehicule_assignation(elem[0]) !=[] and get_fleet_vehicule_assignation(elem[0]) is not None:
+            print("Got some updates to be done on get_fleet_vehicule_assignation")
+            datas_fleet_vehicule_assign= get_fleet_vehicule_assignation(elem[0])
+            driver_id=elem[0]         
+            id_vehicule_prec, id_vehicule_actual, start_date_vehic_prec, start_date_vehic_actual, end_date_vehic_prec, end_date_vehic_actual, last_update_vehicule_prec, last_update_vehicule_actual=datas_fleet_vehicule_assign
+            half_flow_odoo_topivotdb.two_min_flux_vehicules_fleet_vehicule_assignation_drivers_datas_update( 
+              driver_id, id_vehicule_prec, start_date_vehic_prec, end_date_vehic_prec, id_vehicule_actual, 
+              start_date_vehic_actual, end_date_vehic_actual, last_update_vehicule_prec, last_update_vehicule_actual)
+        else:
+            print("No update performed at this time")
+        
     
     
 '''
